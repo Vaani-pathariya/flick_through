5ad0bd825b0cd0e88bb4286177c94ae127374551
Vaani <vaanipathariya@gmail.com>
2023-03-02 02:10:28 +0530

    feat: Making the layout of landing page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,14 @@
 import "./main.css"
+import logo from "../logo.jpeg"
+import { Outlet } from "react-router-dom";
 const Main=()=>{
     return(
         <div>
-            <nav className="main">
-                hello
+            <nav className="main-navbar">
+                <img src={logo}></img>
+                <hr></hr>
+                <h3>Blogs</h3>
+                <Outlet></Outlet>
             </nav>
         </div>
     )
